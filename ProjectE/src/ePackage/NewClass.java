c0999c62a05400cbcf4c43886fce8d3315f2fe62
Prasann20 <121236536+Prasann20@users.noreply.github.com>
2023-02-09 20:52:00 +0530

    First commit

---
+++ ProjectE/src/ePackage/NewClass.java
@@ -0,0 +1,12 @@
+package ePackage;
+
+public class NewClass {
+
+	public static void main(String[] args) 
+	{
+	   System.out.println("This is my new repo");
+	   System.out.println("First commit");
+
+	}
+
+}
